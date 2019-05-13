725fd50e3e03802a7e2fc4c9c75972f2db7a1657
dangbandy <dangbandy@gmail.com>
2019-05-13 10:18:38 -0700

    Created GUI using Qt

---
+++ QT/Legv8Interpreter/mainwindow.h
@@ -0,0 +1,101 @@
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "instruction.hpp"
+using namespace std;
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QMenu;
+class QDockWidget;
+class QListWidgetItem;
+class QPlainTextEdit;
+class QListWidget;
+class QSessionManager;
+
+QT_END_NAMESPACE
+
+//! [0]
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+
+    void loadFile(const QString &fileName);
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void itemClicked(QListWidgetItem * i);
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void about();
+    void documentWasModified();
+#ifndef QT_NO_SESSIONMANAGER
+    void commitData(QSessionManager &);
+#endif
+
+private:
+    void createDockWindows();
+    void createActions();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool maybeSave();
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
+    QString curFile;
+    QMenu *viewMenu;
+    QPlainTextEdit *textEdit;
+    QListWidget *memoryList;
+    QListWidget *registerList;
+    QDockWidget *registerDock;
+    QDockWidget *memoryDock;
+
+    void parse(string filename);
+    void resetMem();
+    void editMemory();
+
+    void execCommand();
+    void initSingle();
+    void execSingle();
+
+    void findLabel(string label);
+
+    void execR1(int type);
+    void execR2(int type);
+    void execI1(int type);
+    void execI2(int type);
+    void execB(int type);
+    void execCB(int type);
+    void execD1(int type);
+    void execD2(int type);
+    void execBL(int type);
+    void execBR(int type);
+
+    int mode = 0;
+
+    vector<Instruction> PGM;
+    long long int RFILE[32];
+    char *MEM = new char[1000];
+    char *STACK= new char[1000];
+
+    int N = 0;
+
+    int currentSFlagInstr = -1;
+    int ConditionReg1 = 0;
+    int ConditionReg2 = 0;
+
+    int Zero = 0, Overflow = 0, Negative = 0, Carry = 0;
+};
+//! [0]
+
+#endif
---
+++ QT/Legv8Interpreter/release/moc_mainwindow.cpp
@@ -0,0 +1,149 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[13];
+    char stringdata0[122];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 11), // "itemClicked"
+QT_MOC_LITERAL(2, 23, 0), // ""
+QT_MOC_LITERAL(3, 24, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(4, 41, 1), // "i"
+QT_MOC_LITERAL(5, 43, 7), // "newFile"
+QT_MOC_LITERAL(6, 51, 4), // "open"
+QT_MOC_LITERAL(7, 56, 4), // "save"
+QT_MOC_LITERAL(8, 61, 6), // "saveAs"
+QT_MOC_LITERAL(9, 68, 5), // "about"
+QT_MOC_LITERAL(10, 74, 19), // "documentWasModified"
+QT_MOC_LITERAL(11, 94, 10), // "commitData"
+QT_MOC_LITERAL(12, 105, 16) // "QSessionManager&"
+
+    },
+    "MainWindow\0itemClicked\0\0QListWidgetItem*\0"
+    "i\0newFile\0open\0save\0saveAs\0about\0"
+    "documentWasModified\0commitData\0"
+    "QSessionManager&"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       8,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   54,    2, 0x08 /* Private */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    0,   58,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    0,   60,    2, 0x08 /* Private */,
+       9,    0,   61,    2, 0x08 /* Private */,
+      10,    0,   62,    2, 0x08 /* Private */,
+      11,    1,   63,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 12,    2,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->itemClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 1: _t->newFile(); break;
+        case 2: _t->open(); break;
+        case 3: { bool _r = _t->save();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 4: { bool _r = _t->saveAs();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 5: _t->about(); break;
+        case 6: _t->documentWasModified(); break;
+        case 7: _t->commitData((*reinterpret_cast< QSessionManager(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 8;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
