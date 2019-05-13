e3becd537b814793535807bd3e6b17f1e95a97ee
dangbandy <dangbandy@gmail.com>
2019-05-13 16:26:59 -0700

    finished

--- makefile
+++
@@ -1,4 +0,0 @@
-a.out: main.cpp instruction.hpp
-	g++ -std=c++11 main.cpp instruction.hpp
-run: a.out
-	./a.out
--- testSubProcess.py
+++
@@ -1,7 +0,0 @@
-import shlex, subprocess
-
-command_line = "./a.out"
-args = shlex.split(command_line)
-p = subprocess.Popen(args, shell = True)
-
-print("")
